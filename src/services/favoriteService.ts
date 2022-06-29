c085062a31e7d0277a065ba974a8395a4c690a95
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-06-28 23:36:45 -0300

    Obtendo os cursos favoritos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,23 +1,53 @@
-import { Favorite } from "../models/Favorite"
+import { Favorite } from "../models"
 
 export const favoriteService = {
-	create: async (profileId: number, courseId: number) => {
-    const favoriteAlreadyExists = await Favorite.findOne({
-      where: {
-        profileId,
-        courseId
+  findByUserId: async (userId: number) => {
+    const favorites = await Favorite.findAll({
+      attributes: [['user_id', 'userId']],
+      where: { userId },
+      include: {
+        association: 'Course',
+        attributes: [
+          'id',
+          'name',
+          'synopsis',
+          ['thumbnail_url', 'thumbnailUrl']
+        ]
       }
     })
 
-    if (favoriteAlreadyExists) {
-      throw new Error('Curso já existente na lista')
+    return {
+      userId,
+      courses: favorites.map(favorite => favorite.Course)
     }
+  },
 
-    const favorite = await Favorite.create({
-      profileId,
-      courseId
+  create: async (userId: number, courseId: number) => {
+    const favorite = Favorite.create({
+      courseId,
+      userId
     })
 
     return favorite
   },
+
+  delete: async (userId: number, courseId: number) => {
+    await Favorite.destroy({
+      where: {
+        userId,
+        courseId
+      }
+    })
+  },
+
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return favorite !== null
+  }
 }
